05a0c68bd4d2b2cf8596ef9d7d685f48be33470e
xiaochenggong <3240868373@qq.com>
2020-04-26 17:14:30 +0800

    日志模块加上日志

--- ATM-练习1/core/logger.py
+++ ATM-练习1/core/logger.py
@@ -12,11 +12,17 @@ import logging
 
 def logger():
     logger = logging.Logger('alex', level=logging.DEBUG)
+    # 创建一个handler,用于写入到日志文件
     file_handler = logging.FileHandler(r'..\\logs\\bank.log',encoding='utf-8')
+    # 创建一个handler,用于输出到控制台
     ch_handler = logging.StreamHandler()
+    # 设置日志格式
     formatter = logging.Formatter('%(asctime)s %(name)s %(message)s', datefmt='%Y%m%d %H:%M:%S')
+    # 设置输出到日志文件中的格式
     file_handler.setFormatter(formatter)
+    # 设置输出到控制台中的格式
     ch_handler.setFormatter(formatter)
+    # logger 添加文件handler对象和控制台handler 对象
     logger.addHandler(file_handler)
     logger.addHandler(ch_handler)
 
